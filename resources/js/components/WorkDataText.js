42345fa29fa9bd98f72be4e5bb67530a4e5cb33e
akez <1791189210@qq.com>
2021-08-11 09:05:04 +0800

    grid

--- resources/js/components/WorkDataText.js
+++ resources/js/components/WorkDataText.js
@@ -2,20 +2,19 @@ import React from 'react';
 import GridLayout from 'react-grid-layout';
 
 const WorkDataText = () => {
-    const layout = [
-        {i: 'a', x: 0, y: 0, w: 6, h: 2, static: true},
-        {i: 'b', x: 6, y: 0, w: 6, h: 2, static: true},
-        {i: 'c', x: 0, y: 2, w: 6, h: 2, static: true},
-        {i: 'd', x: 6, y: 2, w: 6, h: 2, static: true},
-    ];
+    /*const layout = [
+        {i: 'a', x: 0, y: 0, w: 6, h: 6, static: true},
+        {i: 'b', x: 6, y: 0, w: 6, h: 6, static: true},
+        {i: 'c', x: 0, y: 2, w: 6, h: 6, static: true},
+        {i: 'd', x: 6, y: 2, w: 6, h: 6, static: true},
+    ];*/
     return (
-        <div>
+        <div style={{width: '100%', height: '100%'}}>
             <p>this is work data text.</p>
-            <GridLayout layout={layout} cols={12} rowHeight={6}>
-                <div key="a">a</div>
-                <div key="b">b</div>
-                <div key="c">c</div>
-                <div key="d">d</div>
+            <GridLayout className="layout" cols={12} rowHeight={30} width={1200}>
+                <div key="a" data-grid={{x: 0, y: 0, w: 6, h: 2, static: true}}>a</div>
+                <div key="b" data-grid={{x: 6, y: 0, w: 6, h: 2, minW: 2, maxW: 4}}>b</div>
+                <div key="c" data-grid={{x: 4, y: 0, w: 6, h: 2}}>c</div>
             </GridLayout>
         </div>
     );
